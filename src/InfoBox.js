3ee31200f86fee3e802a8285de78d148de0e31b0
Josh Guha <joshguha@gmail.com>
2020-04-09 11:42:59 +0100

    Cards added to InfoBox

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -1,3 +1,22 @@
 import React from "react";
 import { Card, CardContent, Typography } from "@material-ui/core";
 import "./InfoBox.css";
+
+function InfoBox({ title, cases, total, active, isRed, ...props }) {
+    console.log(title, active);
+    return (
+      <Card
+        onClick={props.onClick}
+        className={`infoBox ${active && "infoBox--selected"} ${
+          isRed && "infoBox--red"
+        }`}
+      >
+        <CardContent>
+  
+        </CardContent>
+      </Card>
+    );
+  }
+  
+  export default InfoBox;
+  
