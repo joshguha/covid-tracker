f044fb73afa0bf33c48df4d4fc7e70956e279e16
Josh Guha <joshguha@gmail.com>
2020-04-13 11:45:57 +0100

    Added map component

---
+++ src/Map.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Map as LeafletMap, TileLayer } from "react-leaflet";
+import "./Map.css";
+import { showDataOnMap } from "./util";
+
+function Map({ countries, casesType, center, zoom }) {
+  return (
+    <div className="map">
+
+    </div>
+  );
+}
+
+export default Map;
