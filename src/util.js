5e89ab67a6c74f6787aeb38775a7eeea5b69d1bd
Josh Guha <joshguha@gmail.com>
2020-04-14 11:47:27 +0100

    Created prettyPrintStat and showDataOnMap util fns

--- src/util.js
+++ src/util.js
@@ -22,3 +22,18 @@ const casesTypeColors = {
     multiplier: 2000,
   },
 };
+
+export const sortData = (data) => {
+    let sortedData = [...data];
+    sortedData.sort((a, b) => {
+      if (a.cases > b.cases) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+    return sortedData;
+  };
+  
+  export const prettyPrintStat = (stat) =>
+    stat ? `+${numeral(stat).format("0.0a")}` : "+0";
